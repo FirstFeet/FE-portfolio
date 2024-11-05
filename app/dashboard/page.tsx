121d1a92ee15e8cf7e0cf3dbfc545dd8be18b6ab
Narayanaraj Pandurangam <narayanaraj.k@photon.in>
2024-11-05 12:58:43 +0530

    API call fixes

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,11 +4,11 @@ import ExperienceTile from "@/components/ExperienceTile"
 
 export default async function Page() {
 
-    let data = await fetch(`${API_URL}/project-data`)
+    let data = await fetch(`${API_URL}/project-data`, { next: { revalidate: 3600 } })
     let projects = await data.json()
     return (
         <>
-            {projects?.default?.map((company: any,index: number) => {
+            {projects.map((company: any,index: number) => {
                 return (
                     <ExperienceTile key={index} title={company.Company} projects={company.ProjectList}/>
                 )
