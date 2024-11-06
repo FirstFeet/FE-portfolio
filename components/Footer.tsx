f60316876cce3372985c7559600b3ad88327e836
Narayanaraj Pandurangam <narayanaraj.k@photon.in>
2024-11-06 16:33:56 +0530

    Work History correction

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,7 +9,6 @@ const Footer = () => {
   return (
     <div className="m-0">
       <div className="bg-[#0a023a] text-[#b9b9de] md:flex  md:justify-around md:text-sm items-center md:px-1 px-2 md:mt-1 md:m-0 w-full grid grid-cols-1 gap-3  py-3 mt-1">
-       
         <div className="flex flex-wrap flex-col md:flex-row justify-center items-center">
             <PiCopyrightDuotone className="w-12 h-12 rounded-full p-2" />
             <p className="text-xl font-semibold pl-2">K.Narayanaraj.</p>
